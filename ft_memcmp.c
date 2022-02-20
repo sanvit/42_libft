3a35a65e5adaeffad231be5db627a8c28aa7cbad
Jaewon Kim <sanvit@sanvit.me>
2022-02-20 13:10:16 +0000

    fix memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -21,6 +21,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	s1_c = (unsigned char *)s1;
 	s2_c = (unsigned char *)s2;
+	if (!n)
+		return (0);
 	while (n--)
 	{
 		if (s1_c[i] != s2_c[i])
